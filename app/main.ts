c44c95a1c39019d7ecd482a963268a5a4e0c90e1
Noel Felix <nn.felix.25@gmail.com>
2026-08-04 22:30:51 -0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,21 +1,41 @@
 import { createInterface } from "readline";
 
 const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: "$ ",
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "$ ",
 });
 
 // TODO: Uncomment the code below to pass the first stage
 rl.prompt();
 
-const inputListener: Parameters<typeof rl.on>[1] = input => {
-    handleCommandNotFound(input);
-    rl.prompt();
-}
+const inputListener: Parameters<typeof rl.on>[1] = (command) => {
+    dispatch(command);
 
-rl.on('line', inputListener)
+    // Prompt the user for the next command if there are still listeners for the "line" event
 
-function handleCommandNotFound(input: string) {
-    console.log(`${input}: command not found`);
+    if (rl.listenerCount("line") > 0) {
+        rl.prompt();
+    }
+};
+
+const closeListener: Parameters<typeof rl.on>[1] = () => {
+    rl.removeListener("line", inputListener);
+};
+
+rl.on("line", inputListener);
+rl.on("close", closeListener);
+
+function dispatch(input: string) {
+    switch (input) {
+        case "exit": {
+            rl.close();
+            return;
+        }
+
+        default: {
+            console.log(`${input}: command not found`);
+            return;
+        }
+    }
 }
