ccfb82cf248d2116a4ab1a58404783b7b5f2371f
Noel Felix <nn.felix.25@gmail.com>
2026-08-09 15:53:07 -0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -20,6 +20,7 @@ const inputListener: Parameters<typeof rl.on>[1] = (input) => {
 
   // Prompt the user for the next command if there are still listeners for the "line" event
   if (rl.listenerCount("line") > 0) {
+    console.log("prompt");
     rl.prompt();
   }
 };
@@ -107,9 +108,11 @@ async function dispatch(command: string, args: string[]) {
       const executablePath = parseExecutablePath(command);
       if (executablePath) {
         const TEMP_ASYNC_WRAPPER = await new Promise((res, _) => {
+          console.log("wrapper");
           exec(
             `${executablePath} ${args.join(" ")}`,
             (error, stdout, stderr) => {
+              console.log("exec callback");
               res(console.log(error, stdout, stderr));
             },
           );
